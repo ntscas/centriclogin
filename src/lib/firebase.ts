dcf64006f50dc9558e748aba8f9565a47a302ed6
ntscas <ntscas@gmail.com>
2026-06-03 19:55:55 +0900

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ import firebaseConfig from '../../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 
 const provider = new GoogleAuthProvider();
 // Request Google Sheets and Drive file creation scopes
